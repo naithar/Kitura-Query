a04a67fd6ba5131e6ab84190a9d778d9d0a8be0d
Sergey Minakov <devias.naith@gmail.com>
2017-01-12 18:29:53 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "KituraQuery",
     dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1)
         .Package(url: "https://github.com/naithar/Query.git", majorVersion: 0, minor: 0)
     ]
 )
