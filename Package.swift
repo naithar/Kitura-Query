87e43aa91cdc053ac08d724665703d6269872279
Sergey Minakov <devias.naith@gmail.com>
2017-02-28 13:35:18 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "KituraQuery",
     dependencies: [
-        .Package(url: "https://naithar@bitbucket.org/naithar/kitura-session-clone.git", majorVersion: 1),
+        .Package(url: "https://naithar@bitbucket.org/naithar/kitura-clone.git", majorVersion: 1),
         .Package(url: "https://github.com/naithar/Query.git", majorVersion: 0)
     ]
 )
