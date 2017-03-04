0437be87211956d5dc7d8383a7927abe9b7d39f4
Naithar <devias.naith@gmail.com>
2017-03-04 17:03:42 +0300

    Remove unused files. Separated `Container` class and Parsers in files.

---
+++ Sources/KituraQuery/Parser.swift
@@ -0,0 +1,51 @@
+//
+//  Parser.swift
+//  KituraQuery
+//
+//  Created by Sergey Minakov on 04.03.17.
+//
+//
+
+import Foundation
+import SwiftyJSON
+import Wrap
+import Query
+
+public protocol RawBodyParserProtocol {
+    
+    func parse(raw data: Data) -> Wrap.Value
+}
+
+class JSONParser: RawBodyParserProtocol {
+    
+    func parse(raw data: Data) -> Wrap.Value {
+        let json = JSON(data: data)
+        
+        if case .null = json.type { return .null }
+        
+        return Wrap.Value(json)
+    }
+}
+
+class TextParser: RawBodyParserProtocol {
+    
+    func parse(raw data: Data) -> Wrap.Value {
+        guard let string = String(data: data, encoding: .utf8) else { return .null }
+        return Wrap.Value(string)
+    }
+}
+
+class QueryParser: RawBodyParserProtocol {
+    
+    func parse(raw data: Data) -> Wrap.Value {
+        guard let query = String(data: data, encoding: .utf8) else { return .null }
+        return Query.init(percentEncodedQuery: query)
+    }
+}
+
+class MultipartParser: RawBodyParserProtocol {
+    
+    func parse(raw data: Data) -> Wrap.Value {
+        return .null
+    }
+}
