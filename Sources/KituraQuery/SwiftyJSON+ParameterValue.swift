229f6331147e7d63ce834a7c6d736722a93b6a9a
Naithar <devias.naith@gmail.com>
2017-03-04 15:56:41 +0300

    SwiftyJSON update for Wrap module

--- Sources/KituraQuery/SwiftyJSON+ParameterValue.swift
+++ Sources/KituraQuery/SwiftyJSON+ParameterValue.swift
@@ -20,6 +20,13 @@ import Wrap
 import Kitura
 import SwiftyJSON
 
+extension WrapKeyProtocol {
+    
+    public var jsonKey: JSONSubscriptType? {
+        return self as? JSONSubscriptType
+    }
+}
+
 extension JSON: WrapConvertible {
 //
 //    public var data: Data? {
@@ -35,17 +42,77 @@ extension JSON: WrapConvertible {
         return dictionary as [AnyHashable : Any]
     }
 }
-//
-//    public subscript(keys: [QueryKeyProtocol]) -> ParameterValue {
-//        get {
-//            let keys = keys.flatMap { $0.jsonKey }
-//            return self[keys]
-//        }
-//    }
-//    
-//    public subscript(keys: QueryKeyProtocol...) -> ParameterValue {
-//        get {
-//            return self[keys]
-//        }
-//    }
-//}
+
+extension JSON: WrapSubscriptable {
+    
+    public subscript(keys: WrapKeyProtocol) -> JSON {
+        guard let key = keys.jsonKey else { return JSON.null }
+        return self[key]
+    }
+}
+
+extension JSON: WrapCheckable {
+    
+    public var isBool: Bool {
+        switch self.type {
+        case .bool:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isInt: Bool {
+        switch self.type {
+        case .number:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isDouble: Bool {
+        switch self.type {
+        case .number:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isString: Bool {
+        switch self.type {
+        case .string:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isArray: Bool {
+        switch self.type {
+        case .array:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isDictionary: Bool {
+        switch self.type {
+        case .dictionary:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var isNull: Bool {
+        switch self.type {
+        case .null:
+            return true
+        default:
+            return false
+        }
+    }
+}
