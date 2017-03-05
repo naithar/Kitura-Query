43fa939b50e830749a1600cdec9d43d54bc99a09
Naithar <devias.naith@gmail.com>
2017-03-05 10:28:38 +0300

    Multipart implementation progress

--- Sources/KituraQuery/Parser.swift
+++ Sources/KituraQuery/Parser.swift
@@ -11,6 +11,38 @@ import SwiftyJSON
 import Wrap
 import Query
 
+extension String {
+    
+    static let dashes = "--"
+    static let newLine = "\r\n"
+}
+
+extension Data {
+    
+    func hasPrefix(_ data: Data) -> Bool {
+        guard data.count <= self.count else { return false }
+        return self.subdata(in: 0..<data.count) == data
+    }
+    
+    func components(separatedBy separator: Data) -> [Data] {
+        var parts: [Data] = []
+        
+        var search: Range = 0..<self.count
+        while true {
+            // search for the next occurence of the separator
+            guard let found = self.range(of: separator, in: search) else {
+                parts.append(self.subdata(in: search))
+                break
+            }
+            // add a part up to the found location
+            parts.append(self.subdata(in: search.lowerBound..<found.lowerBound))
+            
+            search = found.upperBound..<self.count
+        }
+        return parts
+    }
+}
+
 public protocol RawBodyParserProtocol {
     
     func parse(raw data: Data, type: String?) -> Wrap.Value
@@ -41,17 +73,56 @@ class QueryParser: RawBodyParserProtocol {
     }
 }
 
+public struct MultipartData {
+    
+    public var name: String
+    public var fileName: String?
+    public var content: Wrap.Value
+    
+    init(name: String, content: Wrap.Value, fileName: String? = nil) {
+        self.name = name
+        self.content = content
+        self.fileName = fileName
+    }
+}
+
 class MultipartParser: RawBodyParserProtocol {
     
     func parse(raw data: Data, type: String?) -> Wrap.Value {
         guard let type = type,
-            let boundary = self.boundary(from: type) else { return .null }
+            let boundary = self.boundary(from: type),
+            let boundaryData = self.boundaryData(using: boundary) else { return .null }
         
-        return .null
+        
+        //body["name"].as(Part.self) -> Part
+        
+        //Part -> query, file, text, json
+    
+        
+        //Part["p"] -> query["p"], json["p"]. file -> nil. text -> nil
+        
+        let parts = data.components(separatedBy: boundaryData.boundary)
+        
+        var result: [Wrap.Value] = []
+        var encounteredFinish = false
+        
+        for part in parts {
+            
+            guard !part.hasPrefix(boundaryData.finish) else {
+                encounteredFinish = true
+                break
+            }
+            
+            if let multipartItem = self.getPart(from: part) {
+                result.append(multipartItem)
+            }
+        }
+        
+        return encounteredFinish ? Wrap.Value(result) : .null
     }
     
     func boundary(from type: String) -> String? {
-        guard let boundryIndex = type.range(of: "boundary=") else { nil }
+        guard let boundryIndex = type.range(of: "boundary=") else { return nil }
         
         var boundary = type
             .substring(from: boundryIndex.upperBound)
@@ -62,5 +133,18 @@ class MultipartParser: RawBodyParserProtocol {
         }
         
         return boundary
+    }
+    
+    func boundaryData(using boundary: String) -> (boundary: Data, newLine: Data, headerFinish: Data, finish: Data)? {
+        guard let boundary = (String.dashes + boundary).data(using: .utf8),
+            let newLine = String.newLine.data(using: .utf8),
+            let headerFinish = (String.newLine + String.newLine).data(using: .utf8),
+            let finish = String.dashes.data(using: .utf8) else { return nil }
+        
+        return (boundary, newLine, headerFinish, finish)
+    }
+    
+    private func getPart(from partData: Data) -> Wrap.Value? {
+        return nil
     }
 }
