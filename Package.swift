12454c7cb062a3b69a08d5b0f6e36058c0d176c4
Sergey Minakov <s.minakov@be-interactive.ru>
2017-03-10 16:46:19 +0300

    package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "KituraWrap",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+        .Package(url: "https://naithar@bitbucket.org/naithar/kitura-clone.git", majorVersion: 1),
         .Package(url: "https://github.com/naithar/Query.git", majorVersion: 0, minor: 1)
     ]
 )
